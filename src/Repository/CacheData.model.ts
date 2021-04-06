84d8ad321b858c90657cf94796b959d383426cee
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-04-07 00:02:23 +0430

    Modify to remove ttl from model

--- src/Repository/CacheData.model.ts
+++ src/Repository/CacheData.model.ts
@@ -4,18 +4,15 @@ import mongoose, { Document, Model, model, Schema } from 'mongoose';
  * Interface to model the CacheData Schema for TypeScript.
  * @param key: string
  * @param data: object
- * @param ttl: number
  */
 export interface ICacheData extends Document {
   key: string;
   data: object;
-  ttl: number;
 }
 
 interface CacheDataAttrs {
   key: string;
   data: object;
-  ttl: number;
 }
 
 interface CacheDataModel extends Model<ICacheData> {
@@ -31,11 +28,6 @@ const cacheDataSchema: Schema = new Schema({
   data: {
     type: Object,
     required: true,
-  },
-  ttl: {
-    type: Number,
-    required: true,
-    default: 60000,
   },
   date: {
     type: Date,
