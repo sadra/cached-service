082ce6032df1e774d7c52f3f6e90b3095888720d
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-04-06 21:17:35 +0430

    Modify router to add Controller and Service for CacheData

--- src/Controllers/router.ts
+++ src/Controllers/router.ts
@@ -1,7 +1,15 @@
+import CacheController from './cache.controller';
 import { IndexController } from './index.controller';
 import { Application, Router } from 'express';
+import GetCacheData from '../Server/GetCacheData.service';
+import CacheDataRepository from '../Repository/CacheData.repository';
 
-const _routes: [string, Router][] = [['/', IndexController]];
+const getCacheData = new GetCacheData(new CacheDataRepository());
+
+const _routes: [string, Router][] = [
+  ['/', IndexController],
+  ['/cache', new CacheController(getCacheData).routes()],
+];
 
 export const routes = (app: Application) => {
   _routes.forEach((route) => {
