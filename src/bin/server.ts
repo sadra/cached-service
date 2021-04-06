8b2ff0fc100c903fbe8dd62600fabf26650e7d12
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-04-06 20:13:33 +0430

    Add mongoose package, Add Database Handler to connect the MongoDB

--- src/bin/server.ts
+++ src/bin/server.ts
@@ -1,6 +1,9 @@
 import { app } from './app';
+import connectDB from '../Repository/Database.handler';
 
 const PORT = process.env.PORT || 8000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
