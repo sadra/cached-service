ee3e2eb75ddff6ba6a3c4c901609bc1af16385dc
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2021-04-06 23:50:24 +0430

    Modify to add body-parser

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -1,8 +1,12 @@
 import express, { Application } from 'express';
+import { json } from 'body-parser';
 import { routes } from '../Controllers/router';
 
 // Boot express
 export const app: Application = express();
+
+// Body Parser
+app.use(json());
 
 // Application routing
 routes(app);
